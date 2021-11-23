94b0a046d14e93006f4eae6b2bb6f0d926a9c557
LukeDraper <lukewdraper@gmail.com>
2021-11-23 15:20:18 -0700

    Updated index.ejs to have all elements in search form.

--- controllers/search.js
+++ controllers/search.js
@@ -1,23 +1,27 @@
+const musicianModel = require("../models/musician");
+
 exports.getIndex = (req, res, next) => 
 {
-    let musicians = [
-        {
-            first_name: "Charles Hardin \"Buddy\"",
-            last_name: "Holly",
-            city: "Lubbock, Texas",
-            lead_vocals: true,
-            backup_vocals: true,
-            genres: [],
-            instruments: []
-        },
-        {
+    // let musicians = [
+    //     {
+    //         first_name: "Charles Hardin \"Buddy\"",
+    //         last_name: "Holly",
+    //         city: "Lubbock, Texas",
+    //         lead_vocals: true,
+    //         backup_vocals: true,
+    //         genres: [],
+    //         instruments: []
+    //     },
+    //     {
 
-        }
-    ]
+    //     }
+    // ]
+1
+    const musicians = musicianModel.getMusicians();
   res.render('index', {
       pageTitle: 'Search',
       path: '/',
-      mus: [], //Placeholders to work with index.ejs
+      mus: musicians, //Placeholders to work with index.ejs
       genres: [],
       instruments: []
   });
