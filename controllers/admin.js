54320f57904f560685532e80e67da7d72da19af0
mvrush <mvrush@gmail.com>
2021-12-06 20:14:01 -0700

    Fix Image Path

--- controllers/admin.js
+++ controllers/admin.js
@@ -86,7 +86,7 @@ exports.postAddProfile = (req, res, next) => {
         validationErrors: errors.array() // gets the entire errors.array()
       });
     }
-    const imageUrl = image.path; // this finds that path to our images folder on our system
+    const imageUrl = image.path.replace('public', ''); // this finds that path to our images folder on our system
     // next lines create a new 'musician' constant from the Musician class with the musician info in it. This is based on our Mongoose schema values.
     // the 'firstName:' is the key to our schema, and 'firstName' is our value from the 'const title' above.
     const musician = new Musician({
