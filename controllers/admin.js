13617e6f0a4b98a5194c645bf0eed82b23864995
polsongarrett <garrettpolson94@gmail.com>
2021-12-07 00:10:39 -0700

    Added test edit

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,6 +8,7 @@ const { validationResult } = require('express-validator'); // brings in 'express
 
 // we import the class from the models folder, the musician.js file. We use a capital starting character for classes, hence 'Product'.
 const Musician = require('../models/musician');
+const User = require('../models/user')
 
 
 // this next functions exports 'getAddMusician' to our routes/admin.js file
@@ -119,141 +120,53 @@ exports.postAddProfile = (req, res, next) => {
 };
 
 
-// this next functions exports 'getEditProduct' to our routes/admin.js file
-exports.getEditProduct = (req, res, next) => {
-    // res.render tells router.get to render our 'edit-product.ejs' page. Then it gives it a JavaScript object filled with keynames that have data assigned to them.
-    const editMode = req.query.edit; // this says to look for a query in the url and look at the key/value pair for 'edit'. The key is 'edit' and if the value is 'true' then enable editing.
-    if (!editMode) { // this says if it's not (!) editMode then send a response to redirect back to '/'.
-        return res.redirect('/');
-    }
-    const prodId = req.params.productId; // this pulls the product id from the URL. That product id was added to the url in our routes/admin.js file on line 24.
-    Product.findById(prodId) // Product is a mongoose model and findById is a mongoose method, all built-in. // (old note before Mongoose ->) here we reference our 'Product' class that we required at the top of this page from 'models/product' and we get our prodId from there.
-    .then(product => {
-     // throw new Error('Dummy Error'); // this is a fake error to test our error handling. Uncomment it to test.
-      if (!product) {
-        return res.redirect('/');
-      }
-      res.render('admin/edit-product', {
-        pageTitle: 'Edit Product',
-        path: '/admin/edit-product',
-        editing: editMode,
-        product: product,
-        hasError: false,
-        errorMessage: null,
-        validationErrors: [] // loads an empty array when we 'get' the EditProduct page.
-      });
-    })
-    .catch(err => {
-       console.log("getEditProduct err from controllers/admin.js", err)
-       const error = new Error(err);
-       error.httpStatusCode = 500; // we aren't using this line yet but it bascially sets the value for 'httpStatusCode' to 500.
-       return next(error); // when we use next with error called as an argument, Express will skip all middlwares and move right to an error handling middleware (it's found in our app.js).
-    });
-};
+exports.postEditProfile = (req, res, next) => 
+{
+  console.log("Edit Profile Success");
+  // TODO: replace placholder data with req.body.xyz
+  const userId = "619d67db1e540854b9a69072";
+  const updatedUsername = "BillyBob";
+  const updatedImageUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.rollingstone.com%2Fmusic%2Fmusic-country%2Fbilly-bob-thornton-on-media-lies-musical-truths-and-new-boxmasters-album-106199%2F&psig=AOvVaw1yr6c2gyu1xP2w2Pooue2B&ust=1637728508210000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNDfuaPUrfQCFQAAAAAdAAAAABAJ";
+  const updatedEmail = "bb123xyz@gmail.com";
+  const updatedPassword = "123xyz";
+  const updatedFirstName = "Billy";
+  const updatedLastName = "Bob";
+  const updatedCity = "Montgomery";
+  const updatedCountry = "USA";
+  const updatedLongitude = "";
+  const updatedLatitude = "";
+  const updatedLead_vocals = false;
+  const updatedBackup_vocals = false;
+  const updatedGenres = [{"genreName":"Country"}, {"genreName":"R&B"}, {"genreName":"Rock"}];
+  const updatedInstruments = [{"instrumentName":"Guitar"}, {"instrumentName":"Banjo"}, {"instrumentName":"Piano"}];
 
-exports.postEditProduct = (req, res, next) => {
-    // the following const's pulls the hidden value for name="productId" out of our hidden input on the edit-product.ejs. name="whateverTheNameIs" on the rest of the consts.
-    const prodId = req.body.productId;
-    const updatedTitle = req.body.title;
-    const updatedPrice = req.body.price;
-    const updatedImageUrl = req.body.imageUrl;
-    const updatedDesc = req.body.description;
-    const errors = validationResult(req); // sends a request to 'validationResult' defined above holding our 'express-validation'. Looks for validation errors.
-    
-    if (!errors.isEmpty()) { // says look at 'error' which holds 'validationResult(req)', if isEmpty() is not (!) empty, then run this block.
-      console.log("This is our 'postEditProduct errors array:", errors.array());
-      return res.status(422).render('admin/edit-product', { // status(422) sends the '422 Unprocessable Entity' code. Means it was unable to process the contained instructions.
-        pageTitle: 'Edit Product',
-        path: '/admin/edit-product',
-        editing: true, // we set this to false because we are not editing and don't want to change anything.
-        hasError: true,
-        product: {
-          title: updatedTitle,
-          imageUrl: updatedImageUrl,
-          price: updatedPrice,
-          description: updatedDesc,
-          _id: prodId // we HAVE to send the prodId again so the page loads properly when submitting an edit after an error.
+  User.findByIdAndUpdate(userId, 
+    {
+      username : updatedUsername,
+      imageUrl : updatedImageUrl,
+      email : updatedEmail,
+      password : updatedPassword,
+      musician: {
+        firstName : updatedFirstName,
+        lastName : updatedLastName,
+        location : {
+          city : updatedCity,
+          country : updatedCountry,
+          longitude : updatedLongitude,
+          latitude : updatedLatitude
         },
-        errorMessage: errors.array()[0].msg, // gets the first element out of an array built from 'errors' which hods data from 'validationResult(req).
-        validationErrors: errors.array() // when we post we want the entire errors.array(), not the empty we set we did the 'get' command.
-      });
+        lead_vocals : updatedLead_vocals,
+        backup_vocals : updatedBackup_vocals,
+        genres : updatedGenres,
+        instruments : updatedInstruments
+      }
     }
-
-    // We use mongoose Product and then the findById(prodId) tp fetch our 'product' which it brings in as a full mongoose object that gives us access to our mongoose functions. 
-    // Using our mongoose .save() function it will save our changes to the existing product behind the scenes.
-    Product.findById(prodId).then(product => {
-        if (product.userId.toString() !== req.user._id.toString()) { // if the product userId is not (!) equal to the req.user._id (the logged in user) then don't allow a product edit and send them to the homepage. We use 'toString()' to make sure we're comparing apples to apples.
-          return res.redirect('/');
-        }
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
-      return product.save().then(result => { // we nested this .then block here so it wouldn't execute if they were redirected above. It used to be below this block.
-        console.log('From controllers/admin.js postEditProduct. It say...UPDATED PRODUCT!');
-        res.redirect('/admin/products');
-      });
+  )
+    .then(result => {
+      // console.log(result);
+      res.redirect("/profile");
     })
     .catch(err => {
-       console.log("err from 'postEditProduct' in controllers/admin.js ->", err)
-       const error = new Error(err);
-       error.httpStatusCode = 500; // we aren't using this line yet but it bascially sets the value for 'httpStatusCode' to 500.
-       return next(error); // when we use next with error called as an argument, Express will skip all middlwares and move right to an error handling middleware (it's found in our app.js).
-      });
-};
-
-
-exports.getProducts = (req, res, next) => {
-  // With our Product.find() function we also use it to filter authorization to a specific userId. Limits what products can be seen in the Admin Products page.
-  Product.find( {userId: req.user._id} ) // find() is a mongoose function that gives us all the documents. In it we put a filter and we ask it to only find products from a specific UserId found with 'req.user._id' which is defined in app.js in 'User.findById()'
-  //  .select('title price -_id') // select() allows us to choose which data to display. So here we select the 'title', 'price' and with the minus '-' sign we tell it do not include the id. Commented out because we don't need it.
-  //  .populate('userID') // populate() tells mongoose to populate a certain field with all the detail information and not just the ID. So it will look at 'userId' and gets all detail associated with that userId. Commented out because we don't need it.
-    .then(products => {
-      console.log("These are the products from our 'getProducts' function in the conrollers/admin.js file", products);
-      res.render('admin/products', {
-        prods: products, // keyname is 'prods' for the value of our 'products' array which is defined above with global scope. All the rest follow the same syntax.
-        pageTitle: 'Admin Products',
-        path: '/admin/products'
-      });
-    })
-    .catch(err => {
-      console.log("err from 'getProducts' in controllers/admin.js ->", err)
-      const error = new Error(err);
-      error.httpStatusCode = 500; // we aren't using this line yet but it bascially sets the value for 'httpStatusCode' to 500.
-      return next(error); // when we use next with error called as an argument, Express will skip all middlwares and move right to an error handling middleware (it's found in our app.js).
+      console.log(err);
     });
 };
-
-
-// the following exports deletes a product by calling the built-in 'findByIdAndRemove()' mongoose function.
-exports.postDeleteProduct = (req, res, next) => {
-    const prodId = req.body.productId;
-    Product.deleteOne( {_id: prodId, userId: req.user._id} ) //uses the deleteOne() function and it deletes the prodId but only if the userId is equal to the req.user._id (the user who made the request).
-    .then(() => {
-      console.log('From controllers/admin.js postDeleteProduct. It say...DESTROYED PRODUCT!');
-      res.redirect('/admin/products');
-    })
-    .catch(err => {
-      console.log("err generated from postDeleteProduct found in controllers/admin.js", err)
-      const error = new Error(err);
-      error.httpStatusCode = 500; // we aren't using this line yet but it bascially sets the value for 'httpStatusCode' to 500.
-      return next(error); // when we use next with error called as an argument, Express will skip all middlwares and move right to an error handling middleware (it's found in our app.js).
-    });
-};
-
-
-
-
-
-// Following block now in tuner.js controller. -Matt R.-  
-  // exports.getUserProfile = (req, res, next) => 
-  // {
-  //   res.render('admin/user-profile', {
-  //       pageTitle: 'Profile',
-  //       path: '/views/admin/user-profile',
-  //       // Obvious placeholder code...
-  //       musician: {
-  //         name: "Bill"
-  //       }
-  //   });
-  // };
--- routes/admin.js
+++ routes/admin.js
@@ -14,9 +14,6 @@ const router = express.Router();
 // The following line says "whenever we get a GET request for the route /add-product go ahead and execute the function 'getAddProduct' found in the productsController".
 router.get('/add-profile', isAuth, adminController.getAddProfile); // We use 'isAuth' first because the statement is read left to right and we want to check for an authorized user first. The path for the following will read as '/admin/add-product => GET' because we filtered for it in app.js
 
-// We use 'isAuth' first because the statement is read left to right and we want to check for an authorized user first.
-router.get('/products', isAuth, adminController.getProducts); // the path will read as '/admin/products => GET' because we filtered for it in app.js it automtically adds '/admin' because of our filter in app.js on line 34ish there.
-
 // we use 'app.post' instead of 'app.use' because 'app.post' will only parse post requests. 'app.use' will parse both 'get' and 'post' requests. You can also use 'app.get' to parse only 'get' requests.
 // The following line says "whenever we get a POST request for the route /add-profile go ahead and execute the function 'postAddProduct' found in the productsController".
 // we use the array [] to group our validation statements together. You don't have to use an array but it's convenient for organization.
@@ -65,26 +62,8 @@ router.post('/add-profile',
     adminController.postAddProfile
     );
 
-router.get('/edit-product/:productId', isAuth, adminController.getEditProduct); // a dynamic path segment is indicated by the colon ':'. We then call the adminController and execute the getEditProduct function.
+router.post('/edit-profile', adminController.postEditProfile);
 
-// The next router.post doesn't have a dynamic segment so we use a POST route so data can be enclosed in the request we're sending. 
-// The data will be the updated product info the user filled in. It calls eh postEditProduct function in the adminController.
-router.post('/edit-product',
-[
-    body('title') // calls our 'body' const above to use the 'express-validator' with the following values.
-        .isAscii() // isAscii(str) is a built-in validator that only allows ascii letters, numbers and some special characters.
-        .isLength({ min: 3 }) // sets the minimum length. Could also set max: length as well.
-        .trim(), // trims out whitespace
-    body('imageUrl').isURL(), // validates and sanitizes as a URL. Calls the 'body' const which brings in 'express-validator'.
-    body('price').isFloat(), // calls the 'body' const above to use 'express-validator' and .isFloat() allows decimal numbers
-    body('description')
-        .isLength({ min: 5, max: 400 }) // calls the 'body' const from above on 'description' and sets length minimum and maximum.
-        .trim()
-],
-isAuth,
-adminController.postEditProduct
-);
 
-router.post('/delete-product', isAuth, adminController.postDeleteProduct); // calls the 'postDeleteProduct' function in the adminController.
 
 module.exports = router; // This exports our express module named 'router'. 'router' is a const defined above which holds the 'express.Router()' module.
